538b2649342741e9e4277af21d8f355647f11397
sean-frank <sean@seanpfrank.com>
2025-11-04 20:26:08 -0500

    Added stuffs

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('node:http') 
+const fs = require('node:fs')
+const path = require('node:path')
+const port = 8000
+
+const server = http.createServer(function(req, res){
+    console.log(req.path)
+    res.writeHead(200, { 'Content-Type' : 'text/html' })
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: File Not Found')
+        }
+        else {
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+server.listen(port, function(error){
+    if (error) {
+        console.log('Something went wrong', error)
+    }
+    else {
+        console.log('Server is listening on port: ' + port)
+    }
+})
